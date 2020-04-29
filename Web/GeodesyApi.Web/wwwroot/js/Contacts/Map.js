16c35eb59cb915f8000bbf2350d5da5efb299da2
n3nko0 <nenko_pakov@abv.bg>
2020-04-29 19:12:12 +0300

    Add map with location in contacts

---
+++ Web/GeodesyApi.Web/wwwroot/js/Contacts/Map.js
@@ -0,0 +1,23 @@
+﻿var map = new ol.Map({
+    target: 'map',
+    layers: [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        })
+    ],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([23.331063, 42.684796]),
+        zoom: 17
+    })
+});
+
+var layer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        features: [
+            new ol.Feature({
+                geometry: new ol.geom.Point(ol.proj.fromLonLat([23.331063, 42.684796]))
+            })
+        ]
+    })
+});
+map.addLayer(layer);
